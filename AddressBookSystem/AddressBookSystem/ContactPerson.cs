0a24b1b65095b3db344b43924e597d2cc5d9e626
Ravi Ranjan <92181941+ravi04ez@users.noreply.github.com>
2022-05-27 16:20:32 +0530

    Add New Contact

---
+++ AddressBookSystem/AddressBookSystem/ContactPerson.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    internal class ContactPerson
+    {
+        List<AddressBook> AddressList;
+
+        public ContactPerson()
+        {
+            AddressList = new List<AddressBook>();
+        }
+        public void AddNewContact()
+        {
+            Console.WriteLine("Fill The Details Given Below: ");
+            Console.Write("First Name : ");
+            string First_Name = Console.ReadLine();
+            Console.Write("Last Name : ");
+            string Last_Name = Console.ReadLine();
+            Console.Write(" Address : ");
+            string Address = Console.ReadLine();
+            Console.Write("City : ");
+            string City = Console.ReadLine();
+            Console.Write("State : ");
+            string State = Console.ReadLine();
+            Console.Write("Zip Code : ");
+            int Zip_Code = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Phone Number : ");
+            long Phone_Number = Convert.ToInt64(Console.ReadLine());
+            Console.Write("Email Address : ");
+            string Email_Address = Console.ReadLine();
+
+            AddressBook addressdetails = new AddressBook(First_Name, Last_Name, Address, City, State, Zip_Code, Phone_Number, Email_Address);
+
+
+
+            if (AddressList.Contains(addressdetails))
+            {
+                Console.WriteLine("Details you Enter is Already Present");
+            }
+
+            else
+            {
+                AddressList.Add(addressdetails);
+            }
+        }
+        public void DisplayContactDetails()
+        {
+            foreach (AddressBook address in AddressList)
+            {
+                // printg Details..
+                Console.WriteLine("\nContact Details are :-");
+                Console.WriteLine("Name : {0} {1}", address.First_Name, address.Last_Name);
+                Console.WriteLine("Address : {0}, {1} ({2})", address.Address, address.City, address.State);
+                Console.WriteLine("Zip Code : {0}", address.Zip_Code);
+                Console.WriteLine("Ph. Number : {0}", address.Phone_Number);
+                Console.WriteLine("Email Address : {0}", address.Email_Address);
+            }
+        }
+    }
+}
--- AddressBookSystem/AddressBookSystem/Program.cs
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -10,14 +10,40 @@ namespace AddressBookSystem
             // printing Welcome Message
             Console.WriteLine("\t\t\t******* WELCOME TO ADDRESS BOOK SYSTEM PROGRAM ******");
 
-            // creating Instance/Object of Class AddressBook & setting Parameters value..
-            AddressBook objdetails = new AddressBook("Ravi", "Ranjan", "DLF Phase 4", "Gurugram", "Haryana", 122002, 9319964621, "Ezekie.em@gmail.com");
+            int Choice;
+            do
+            {
+                Console.WriteLine("0.Exit");
+                Console.WriteLine("1.Get Contact Present in Address Book ");
+                Console.WriteLine("2. Add New Contact to Address Book");
+                Console.Write("Enter Your Choice : ");
+                Choice = Convert.ToInt32(Console.ReadLine());
+                switch (Choice)
+                {
+                    case 1:
+                        // creating Instance/Object of Class AddressBook & setting Parameters value..
+                        AddressBook objdetails = new AddressBook("Ravi", "Ranjan", "DLF Phase 4", "Gurugram", "Haryana", 122002, 9319964621, "Ezekie.em@gmail.com");
 
-            // Calling Method Define in Address Class to Displaying Details.
-            objdetails.DisplayContactDetails();
+                        // Calling Method Define in Address Class to Displaying Details.
+                        objdetails.DisplayContactDetails();
+                        break;
+                    case 2:
+                        ContactPerson objaddnewcontact = new ContactPerson();
+                        objaddnewcontact.AddNewContact();
+                        objaddnewcontact.DisplayContactDetails();
+                        break;
+                    default:
+                        Console.WriteLine(" Enter Correct Choice");
+                        break;
+                }
+
+            }
+            while(Choice != 0);
 
             // To Hold Output Terminal
             Console.ReadKey();
+
+
         }
     }
 }
